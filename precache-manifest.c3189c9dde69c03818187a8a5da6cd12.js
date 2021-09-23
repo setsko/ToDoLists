9e88ad29d09d0d05dbb4ff24bfc6447b23c51037
ASUS-PC <setsko1@gmail.com>
2021-09-24 00:46:34 +0300

    Updates

---
+++ precache-manifest.c3189c9dde69c03818187a8a5da6cd12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000b4aa9880383e7f445d99870e01f00",
+    "url": "/ToDoLists/index.html"
+  },
+  {
+    "revision": "312722fe569c79654146",
+    "url": "/ToDoLists/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "8c10faf74d461ff84466",
+    "url": "/ToDoLists/static/js/2.22a9aa6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoLists/static/js/2.22a9aa6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312722fe569c79654146",
+    "url": "/ToDoLists/static/js/main.3b9c9947.chunk.js"
+  },
+  {
+    "revision": "23d0b6368d48f9bc87e2",
+    "url": "/ToDoLists/static/js/runtime-main.91775268.js"
+  }
+]);
